4d7659fbc09679790a3bf0f9cb25de31ea4b72b5
Ted Kassen <tedgkassen@gmail.com>
2020-07-11 15:51:43 -0700

    Reconnect logic, maybe

--- src/limp/client/rtm.clj
+++ src/limp/client/rtm.clj
@@ -15,6 +15,35 @@
 
 (defrecord Response [channel text])
 
+(defn- build-message-topology
+  [socket & handlers]
+  (let [base-stream
+        (->> socket
+             (ms/throttle 25 100)
+             (ms/transform
+              (comp
+
+               ;; Convert Raw JSON to hashmap
+               (map #(json/read-str % :key-fn keyword))
+
+               ;; Filter out heartbeat messages
+
+               ;; clj-ify types
+               (map #(update % :type (comp keyword inflections/dasherize)))
+
+               ;; Log type to STDOUT
+               (map (fn [message]
+                      (clojure.pprint/pprint message)
+                      message)))))]
+
+    ;; Creates an individual stream for each message handler
+    (map (fn [[pred handler]]
+           (ms/transform
+            (comp
+             (filter pred)
+             (map handler))
+            base-stream)) (partition-all 2 handlers))))
+
 (defn- post-message!
   [socket {:keys [channel text]}]
   (ms/put! socket
@@ -24,59 +53,45 @@
              :text text
              :type "message"})))
 
+(defn- start-heartbeat
+  [socket connected]
+  (while true
+    (let [result @(ms/put! socket
+                           (json/write-str
+                            {:id (get-message-id)
+                             :type "ping"}))]
+      (when-not result
+        (reset! connected false))
+      (Thread/sleep 5000))))
+
 (defn start!
   [& handlers]
-  (let [token
-        (System/getenv "SLACK_TOKEN")
-
-        {:keys [body]}
+  (let [{body :body}
         @(http/get +slack-endpoint+
-                   {:query-params {"token" token}})
+                   {:query-params {"token" (System/getenv "SLACK_TOKEN")}})
 
-        {:keys [ok url] :as body-parsed
-         {:keys [self-id]} :self}
+        {:keys [ok url]}
         (json/read-str (bs/to-string body) :key-fn keyword)
 
-        socket
-        (when ok
-          (try
-            @(http/websocket-client url)
-            (catch Exception e
-              (clojure.pprint/pprint e))))]
-    (when socket
-      (try
-        (let [base-stream
-              (->> socket
-                   (ms/throttle 25 100)
-                   (ms/transform
-                    (comp
+        connect-socket
+        (fn []
+          (when ok
+            @(http/websocket-client url)))]
 
-                     ;; Convert Raw JSON to hashmap
-                     (map #(json/read-str % :key-fn keyword))
-
-                     ;; Filter out heartbeat messages
-                     (remove (fn [{type :type}] (= type "hello")))
-
-                     ;; clj-ify types
-                     (map #(update % :type (comp keyword inflections/dasherize)))
-
-                     ;; Log type to STDOUT
-                     (map (fn [message]
-                            (clojure.pprint/pprint message)
-                            message)))))
-
-              ;; Creates an individual stream of each message handler
-              handler-streams
-              (map (fn [[pred handler]]
-                     (ms/transform
-                      (comp
-                       (filter pred)
-                       (map handler))
-                      base-stream)) (partition-all 2 handlers))]
-
+    (while true
+      (let [socket (connect-socket)
+            handler-streams (apply build-message-topology
+                                   socket
+                                   handlers)
+            connected (atom true)
+            heartbeat (future (start-heartbeat socket connected))]
+        (try
           ;; Publish messages output by handler streams
-          (while true
-            (doseq [handler-stream handler-streams]
-              (if-let [message @(ms/take! handler-stream)]
-                (post-message! socket message)))))
-        (finally (ms/close! socket))))))
+          (try
+            (while @connected
+              (doseq [handler-stream handler-streams]
+                (if-let [message @(ms/take! handler-stream)]
+                  (post-message! socket message))))
+
+            (finally (future-cancel heartbeat)))
+          (finally (ms/close! socket)))))))
